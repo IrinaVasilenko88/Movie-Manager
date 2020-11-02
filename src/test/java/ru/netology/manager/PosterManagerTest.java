acbbd36986586a59d4a782312bbaf9814908cf6e
Irina Vasilenko <vasilenko_i@yahoo.com>
2020-11-02 13:31:19 +0300

    Tests fixed

--- src/test/java/ru/netology/manager/PosterManagerTest.java
+++ src/test/java/ru/netology/manager/PosterManagerTest.java
@@ -19,7 +19,7 @@ public class PosterManagerTest {
     private MovieItem eighth = new MovieItem(8, 8, "eighth", "biography", 17);
     private MovieItem ninth = new MovieItem(9, 9, "ninth", "mystery", 18);
     private MovieItem tenth = new MovieItem(10, 10, "tenth", "documentary", 19);
-
+    private MovieItem eleventh = new MovieItem(11, 11, "eleventh", "detective", 20);
     @BeforeEach
     public void setUp() {
         manager.add(first);
@@ -31,11 +31,11 @@ public class PosterManagerTest {
         manager.add(seventh);
         manager.add(eighth);
         manager.add(ninth);
-        manager.add(tenth);
     }
 
     @Test
-    public void shouldGetAllMoviesInOrder() {
+    public void shouldGetTenMovies() {
+        manager.add (tenth);
         MovieItem[] actual = manager.getAll();
         MovieItem[] expected = new MovieItem[]{tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
         assertArrayEquals(expected, actual);
@@ -44,32 +44,23 @@ public class PosterManagerTest {
     @Test
     public void shouldGetMoviesLessThanTen() {
         PosterManager manager = new PosterManager();
-        MovieItem first = new MovieItem(1, 1, "first", "comedy", 10);
-        MovieItem second = new MovieItem(2, 2, "second", "cartoon", 11);
-        MovieItem third = new MovieItem(3, 3, "third", "thriller", 12);
         manager.add(first);
         manager.add(second);
         manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+        manager.add(sixth);
+        manager.add(seventh);
+        manager.add(eighth);
+        manager.add(ninth);
         MovieItem[] actual = manager.getAll();
-        MovieItem[] expected = new MovieItem[]{third, second, first};
+        MovieItem[] expected = new MovieItem[]{ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
         assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldGetMoviesTen() {
+    public void shouldGetMoviesMoreThanTen() {
         PosterManager manager = new PosterManager();
-        MovieItem first = new MovieItem(1, 1, "first", "comedy", 10);
-        MovieItem second = new MovieItem(2, 2, "second", "cartoon", 11);
-        MovieItem third = new MovieItem(3, 3, "third", "thriller", 12);
-        MovieItem fourth = new MovieItem(4, 4, "fourth", "horror", 13);
-        MovieItem fifth = new MovieItem(5, 5, "fifth", "drama", 14);
-        MovieItem sixth = new MovieItem(6, 6, "sixth", "musical", 15);
-        MovieItem seventh = new MovieItem(7, 7, "seventh", "science-fiction", 16);
-        MovieItem eighth = new MovieItem(8, 8, "eighth", "biography", 17);
-        MovieItem ninth = new MovieItem(9, 9, "ninth", "mystery", 18);
-        MovieItem tenth = new MovieItem(10, 10, "tenth", "documentary", 19);
-        MovieItem eleventh = new MovieItem(11, 11, "eleventh", "documentary", 20);
-
         manager.add(first);
         manager.add(second);
         manager.add(third);
@@ -81,7 +72,7 @@ public class PosterManagerTest {
         manager.add(ninth);
         manager.add(tenth);
         manager.add(eleventh);
-        MovieItem[] actual = manager.getAmountOfMovies();
+        MovieItem[] actual = manager.getAll();
         MovieItem[] expected = new MovieItem[]{eleventh, tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second};
         assertArrayEquals(expected, actual);
 
